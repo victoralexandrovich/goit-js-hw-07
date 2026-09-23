95219c62131d985c2e59240cdc485e1b0a5b4332
Viktor_Alexandrovich <victor.shebarshin@gmail.com>
2026-09-23 18:03:14 +0200

    Complete homework 4

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,14 @@
-function slugify(title) {
-  return title.toLowerCase().split(' ').join('-');
+function isEnoughCapacity(products, containerSize) {
+  let totalCount = 0;
+
+  for (const key in products) {
+    totalCount += products[key];
+  }
+
+  return totalCount <= containerSize;
 }
 
-console.log(slugify('Arrays for beginners'));
-console.log(slugify('English for developer'));
-console.log(slugify('Ten secrets of JavaScript'));
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS'));
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14));
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7));
--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,24 @@
-function filterArray(numbers, value) {
-  const result = [];
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
 
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > value) {
-      result.push(numbers[i]);
-    }
-  }
+  changeUsername(newName) {
+    this.username = newName;
+  },
 
-  return result;
-}
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([1, 2, 3, 4, 5], 4));
-console.log(filterArray([1, 2, 3, 4, 5], 5));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
+
+console.log(profile.getInfo());
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo());
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo());
