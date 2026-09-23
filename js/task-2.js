fee9cdc42e783190ecc32a1fda6d6843ddf84054
Viktor_Alexandrovich <victor.shebarshin@gmail.com>
2026-09-23 17:44:45 +0200

    Complete homework 3

--- js/task-2.js
+++ js/task-2.js
@@ -1,14 +1,11 @@
-function formatMessage(message, maxLength) {
-  if (message.length <= maxLength) {
-    return message;
-  }
-
-  return `${message.slice(0, maxLength)}...`;
+function makeArray(firstArray, secondArray, maxLength) {
+  const combinedArray = [...firstArray, ...secondArray];
+  return combinedArray.slice(0, maxLength);
 }
 
-console.log(formatMessage("Curabitur ligula sapien", 16));
-console.log(formatMessage("Curabitur ligula sapien", 23));
-console.log(formatMessage("Vestibulum facilisis purus nec", 20));
-console.log(formatMessage("Vestibulum facilisis purus nec", 30));
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3));
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4));
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3));
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2));
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4));
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0));
